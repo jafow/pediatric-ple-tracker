0bd252ce673f4f02f22bacaf3f9e77a5b6ba40a0
jafow <jared.a.fowler@gmail.com>
2016-11-29 14:12:12 -0800

    parse input into a dict and add columns list for easy input reference
    ignore test db

--- cli.py
+++ cli.py
@@ -1,6 +1,19 @@
 import argparse
 import sqlite3
-conn = sqlite3.connect('tracker.db')
+conn = sqlite3.connect('ple-tracker.sqlite')
+c = conn.cursor()
 
-weight = input('Weight in lbs :')
+# Readline input
+entry_dict = {
+    weight: float(input('Weight in lbs :')),
+    arm_size: float(input('Arm size in cm: ')),
+    ab_size: float(input('Abdomen size in cm: ')),
+    poo_count: int(input('Number of BMs ')),
+    vomit: int(input('Vomiting? ')),
+    injection: int(input('injection? ')),
+    notes: str(input('Include other notes: '))
+}
 
+columns = ['weight', 'arm_size', 'abdomen_size', 'bm_count', 'vomit', 'injection', 'notes']
+c.execute('''CREATE TABLE IF NOT EXISTS ple_measurements (date TEXT, weight REAL, arm_size REAL, abdomen_size REAL, bm_count INTEGER, vomit INTEGER default 0, injection INTEGER default 0, notes TEXT default NULL)''')
+c.commit()
